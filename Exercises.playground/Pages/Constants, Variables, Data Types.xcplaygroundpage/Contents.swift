60be89e7a911519d105ec319fc687d04f1378732
Matthew Amaral <mamaral@lcs-mba3204.lcs.on.ca>
2017-12-11 15:05:43 -0500

    Finished the Constants and Variables

--- Exercises.playground/Pages/Constants, Variables, Data Types.xcplaygroundpage/Contents.swift
+++ Exercises.playground/Pages/Constants, Variables, Data Types.xcplaygroundpage/Contents.swift
@@ -116,7 +116,7 @@ print("the reason this does not run is becasue there is a syntax error because t
 // Answer Question 7 below
 
 
-
+print("Type-safe languages encourage or require you to be clear about the types of values your code can work with. For example, if part of your code expects an Int, you can't pass it a Double or a String.")
 
 //: [Next](@next)
 
